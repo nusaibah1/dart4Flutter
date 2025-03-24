712dbd1ae45b4dbb403e6e446457eef264b04fa4
Nusaibah1 <charifnusaibah01@gmail.com>
2025-03-24 09:44:15 +0200

    Shipping Cost Calculator

--- day_01/hello.dart
+++ day_01/hello.dart
@@ -1,3 +1,36 @@
-void main(){
-  print("Hello, World!");
+// Hello.Dart
+
+// void main(){
+// print("Hello, World!");
+//}
+
+// Excercise
+// Develop a program to calculate the shipping cost based on the destination zone
+import 'dart:io';
+
+void main() {
+  // and the weight of the package (you will be provided)
+  print("Enter your package weight (in kg(s)): ");
+  int? packageWeight = int.parse(stdin.readLineSync()!);
+
+  print("Enter your destination zone: ");
+  String? destinationZone = stdin.readLineSync();
+// if the destination zone is XYZ the shipping cost is $5 per kilogram
+  if (destinationZone == "XYZ") {
+    print(packageWeight * 5);
+    // if the destination zone is ABC  the shipping cost is $7 per kilogram
+  }else if (destinationZone == "ABC") {
+    print(packageWeight * 7);
+    // if the destination zone is PQR the shipping cost is $10 per kilogram
+  } else if (destinationZone == "PQR" ){
+    print(packageWeight * 10);
+  } else {
+    
+
+// ELSE display an error message "Invalid Destination Zone"
+    print("Invalid Destination Zone");
+  }
+
+
+
 }
